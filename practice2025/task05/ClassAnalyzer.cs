1fd5f4a65152d1c046d5ca84c817f3423cb8a673
ShatilovIvan <ivashka1423@mail.ru>
2025-06-27 15:16:20 +0600

    feat: System.Reflection task implementation

---
+++ practice2025/task05/ClassAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿namespace task05;
+
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+public class ClassAnalyzer
+{
+    private Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+
+    public IEnumerable<string> GetPublicMethods()
+        => _type.GetMethods().Where(x => x.IsPublic).Select(x => x.Name);
+
+    public IEnumerable<string> GetMethodParams(string methodName)
+        => _type.GetMethod(methodName).GetParameters().Select(x => x.Name);
+
+    public IEnumerable<string> GetAllFields()
+        => _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).Select(x => x.Name);
+
+    public IEnumerable<string> GetProperties()
+        => _type.GetProperties().Select(x => x.Name);
+
+    public bool HasAttribute<T>() where T : Attribute
+        => _type.GetCustomAttributes<T>().Any();
+}
