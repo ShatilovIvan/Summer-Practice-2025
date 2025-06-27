92b5e9eb44c42f4600abdad0ee52dc587a2bb830
ShatilovIvan <ivashka1423@mail.ru>
2025-06-27 23:57:32 +0600

    style: add last line in files

--- practice2025/task04/ISpaceship.cs
+++ practice2025/task04/ISpaceship.cs
@@ -2,9 +2,9 @@
 
 public interface ISpaceship
 {
-    string MoveForward();      
-    string Rotate(int angle);  
-    string Fire();           
-    int Speed { get; }      
-    int FirePower { get; }   
+    string MoveForward();
+    string Rotate(int angle);
+    string Fire();
+    int Speed { get; }
+    int FirePower { get; }
 }
