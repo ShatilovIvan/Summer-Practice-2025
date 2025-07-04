bcc69a6b8ef89e682f0fab8f25ca103f853412b1
ShatilovIvan <ivashka1423@mail.ru>
2025-07-04 22:07:35 +0600

    feat: FileSystemCommands implementation

---
+++ practice2025/FileSystemCommands/FindFilesCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace FileSystemCommands
+{
+    public class FindFilesCommand : CommandLib.ICommand
+    {
+        public string Path { get; }
+        public string Mask { get; }
+        public List<string> Files { get; private set; } = [];
+
+        public FindFilesCommand(string path, string mask)
+        {
+            Path = path;
+            Mask = mask;
+        }
+
+        public void Execute()
+        {
+            if (!Directory.Exists(Path))
+                throw new DirectoryNotFoundException($"The directory '{Path}' does not exist.");
+
+            Files = Directory.GetFiles(Path, Mask, SearchOption.AllDirectories).ToList();
+        }
+    }
+}
