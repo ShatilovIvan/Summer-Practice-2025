0d32e7dbf6e798dc80355fd011c54ab5ff6ea393
ShatilovIvan <ivashka1423@mail.ru>
2025-07-07 02:19:57 +0600

    feat: plugin loader implementation

---
+++ practice2025/task10/PluginLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using PluginLib;
+
+namespace task10;
+
+public class PluginLoader
+{
+    private static List<Type> _loadedPlugins = new();
+    
+    public static void LoadPlugins(string? pluginsPath)
+    {
+        if (pluginsPath is null)
+            throw new DirectoryNotFoundException("Couldn't find plugins directory!\n");
+
+        var assemblies = Directory.GetFiles(pluginsPath, "*.plugin").Select(Assembly.LoadFrom).SelectMany(s => s.GetTypes()).ToList();
+        var plugins = assemblies.Where(s => s.GetCustomAttribute<PluginLoad>() is not null).ToList();
+
+        plugins.ForEach(s => LoadPlugin(s, plugins));
+    }
+
+    private static void LoadPlugin(Type curPlugin, List<Type> allPlugins)
+    {
+        if (_loadedPlugins.Contains(curPlugin))
+            return;
+
+        var dependencies = curPlugin.GetCustomAttribute<PluginLoad>()?.Dependencies;
+
+        if (dependencies is not null && dependencies.Any())
+            dependencies.Select(depName => allPlugins.FirstOrDefault(s => s.Name == depName)).ToList().ForEach(depType => LoadPlugin(depType, allPlugins));
+
+        _loadedPlugins.Add(curPlugin);
+        var plugin = (IPlugin)Activator.CreateInstance(curPlugin);
+        plugin.Execute();
+    }
+}
---
+++ practice2025/task10tests/PluginLoaderTests.cs
@@ -0,0 +1,24 @@
+﻿namespace task10tests;
+
+using task10;
+
+public class PluginLoaderTests
+{
+    [Fact]
+    public void PluginLoader_ExecutePluginsInCorrectOrder()
+    {
+        var slnDirectory = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.Parent?.FullName;
+
+        if (slnDirectory is null)
+            throw new DirectoryNotFoundException("Couldn't find base directory!\n");
+
+        var stringWriter = new StringWriter();
+        var expectedOutput = "Plugin Three Executed\nPlugin One Executed\nPlugin Two Executed";
+        Console.SetOut(stringWriter);
+
+        var pluginsDirectory = Path.Combine(slnDirectory, "PluginsCompiled");
+        PluginLoader.LoadPlugins(pluginsDirectory);
+
+        Assert.Contains(expectedOutput, stringWriter.ToString());
+    }
+}
